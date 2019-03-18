8b685eeb8730b0e23d5ef7dbc0ad19c469526e26
Thomas Marsh <tmarsh@spotify.com>
2019-03-17 23:00:00 -0400

    some applicative cleanups

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,9 @@ import           Control.Monad.Random        ( Rand
                                              , getRandom
                                              )
 import           Data.Maybe                  ( fromJust, isNothing )
+import           Linear.Metric               ( dot
+                                             , signorm
+                                             )
 import           Linear.V3                   ( V3(..) )
 import           System.IO                   ( hClose
                                              , openFile
@@ -20,8 +23,6 @@ import           System.IO                   ( hClose
                                              )
 import           System.Random               ( newStdGen )
 import qualified Text.Printf                 as P
-
-import Debug.Trace (traceM)
 
 -- from numeric-limits
 maxValue :: (RealFloat a) => a
@@ -94,19 +95,20 @@ lambertian albedo _ (Point p) (Normal normal) = do
     pure $ Just (scattered, albedo)
 
 metal :: RandomGen g => F3 -> ScatterF g
-metal albedo (Ray _ direction) (Point p) (Normal normal) = do
-    let reflected = reflect (unitVec direction) normal
-    let scattered = Ray p reflected
-    pure $ if reflected `dot` normal < 0
-                then Just (scattered, albedo)
-                else Nothing
+metal albedo (Ray _ direction) (Point p) (Normal normal) =
+    pure $ if reflected `dot` normal > 0
+            then Just (scattered, albedo)
+            else Nothing
+    where
+        reflected = reflect (signorm direction) normal
+        scattered = Ray p reflected
 
 data Object g = Object
     { hitF     :: HitF g
     , scatterF :: ScatterF g
     }
 
-newtype Sphere = Sphere (F3, Float)
+newtype Sphere = Sphere (F3, Float)               deriving Show
 
 hitSphere :: RandomGen g => Sphere -> HitF g
 hitSphere (Sphere (center, radius)) r@(Ray origin direction) (tMin, tMax)
@@ -127,9 +129,6 @@ hitSphere (Sphere (center, radius)) r@(Ray origin direction) (tMin, tMax)
                                  (Point pAt)
                                  (Normal ((pAt - center) / mkV radius))
 
-setScatterF :: RandomGen g => ScatterF g -> HitRecord g -> HitRecord g
-setScatterF f (HitRecord t p n _) = HitRecord t p n f
-
 hitList :: RandomGen g => [Object g] -> HitF g
 hitList os r (tMin, tMax)
     = go tMax Nothing os
@@ -137,10 +136,10 @@ hitList os r (tMin, tMax)
         go :: RandomGen g => Float -> Maybe (HitRecord g) -> [Object g] -> Maybe (HitRecord g)
         go _       hr [] = hr
         go closest hr (o:os')
-            = case (hitF o) r (tMin, closest) of
-                Just hr'@(HitRecord t _ _ _)
-                        -> go t (Just $ setScatterF (scatterF o) hr') os'
-                Nothing -> go closest hr  os'
+            = case hitF o r (tMin, closest) of
+                Just (HitRecord t p n _)
+                        -> go t (Just $ HitRecord t p n (scatterF o)) os'
+                Nothing -> go closest hr os'
 
 sphere :: RandomGen g => F3 -> Float -> ScatterF g -> Object g
 sphere pos radius m = Object
@@ -153,25 +152,11 @@ sphere pos radius m = Object
 mkV :: a -> V3 a
 mkV t = V3 t t t
 
-mag :: F3 -> Float
-mag = sqrt . squaredLength
-
-unitVec :: F3 -> F3
-unitVec v = v / (mkV $ mag v)
-
-dot :: F3 -> F3 -> Float
-dot (V3 x1 y1 z1) (V3 x2 y2 z2) = x1*x2 + y1*y2 + z1*z2
-
-squaredLength :: F3 -> Float
-squaredLength (V3 x y z) = x*x + y*y + z*z
-
 pointAtParameter :: Ray -> Float -> F3
-pointAtParameter (Ray a b) t = a + b*(mkV t)
+pointAtParameter (Ray a b) t = a + b*mkV t
 
 reflect :: F3 -> F3 -> F3
 reflect v n = v - mkV (2 * v `dot` n) * n
-
--- Background
 
 color :: RandomGen g
       => [Object g]
@@ -183,33 +168,34 @@ color world depth r@(Ray _ direction)
         Just (HitRecord _ p normal scatter) -> do
             scattered <- scatter r p normal
             if depth >= 50 || isNothing scattered
-                then pure $ Color $ mkV 0
+                then pure $ Color (mkV 0)
                 else do
                     let (r', attenuation) = fromJust scattered
                     (Color bounce) <- color world (depth+1) r'
                     pure $ Color $ attenuation * bounce
         Nothing ->
-            pure $ Color $ (one-t)*one + t*(V3 0.5 0.7 1.0)
+            pure $ Color $ (one-t)*one + t*V3 0.5 0.7 1.0
     where
-        (V3 _ y _) = unitVec direction
+        (V3 _ y _) = signorm direction
         t          = mkV $ 0.5 * (y + 1.0)
         one        = mkV 1.0
         epsilon    = 0.001   -- shadow acne correction
 
 -- https://karthikkaranth.me/blog/generating-random-points-in-a-sphere/
 randomInUnitSphere'ClosedForm :: RandomGen g => Rand g F3
-randomInUnitSphere'ClosedForm = do
-    u <- getRandom
-    v <- getRandom
-    let theta = u * 2 * pi
-    let phi = acos $ 2 * v - 1
-    ri <- getRandom
-    let r = ri ** (1/3)
-    let sinPhi = sin phi
-    let x = r * sinPhi * cos theta
-    let y = r * sinPhi * sin theta
-    let z = r * cos phi
-    pure $ V3 x y z
+randomInUnitSphere'ClosedForm =
+    go <$> getRandom <*> getRandom <*> getRandom
+    where
+        go :: Float -> Float -> Float -> F3
+        go u v r' = V3 x y z
+            where
+                theta  = u * 2 * pi
+                phi    = acos $ 2 * v - 1
+                r      = r' ** (1/3)
+                sinPhi = sin phi
+                x      = r * sinPhi * cos theta
+                y      = r * sinPhi * sin theta
+                z      = r * cos phi
 
 randomInUnitSphere :: RandomGen g => Rand g F3
 randomInUnitSphere = randomInUnitSphere'ClosedForm
@@ -219,17 +205,18 @@ randomInUnitSphere = randomInUnitSphere'ClosedForm
 mkRay :: Camera -> (F3, F3) -> Ray
 mkRay cam (u, v) =
     Ray (camOrigin cam)
-        (lowerLeft cam + u * (horizontal cam) + v * (vertical cam))
+        (lowerLeft cam + u * horizontal cam + v * vertical cam)
 
 coords :: Dimensions -> [Coord]
-coords (Dim (nx, ny)) = [Coord (i, j) | j <- [ny-1,ny-2..0], i <- [0..nx-1]]
+coords (Dim (nx, ny))
+    = (\y x -> Coord (x, y))
+        <$> [ny-1,ny-2..0]
+        <*> [0..nx-1]
 
 coordToUV :: RandomGen g => Dimensions -> Coord -> Rand g (F3, F3)
-coordToUV (Dim (nx, ny)) (Coord (i, j)) = do
-    rx <- getRandom
-    ry <- getRandom
-    pure $ (,) (scale i nx rx)
-               (scale j ny ry)
+coordToUV (Dim (nx, ny)) (Coord (i, j)) =
+    (\rx ry -> (,) (scale i nx rx)
+                   (scale j ny ry)) <$> getRandom <*> getRandom
     where
         scale k z r = mkV $ (r + fromIntegral k) / fromIntegral z
 
@@ -242,17 +229,16 @@ scaleColor (Color (V3 r g b)) = Color3i (f r, f g, f b)
 avgColor :: Int -> [Color] -> Color
 avgColor n cs = Color $ sum (map (\(Color c) -> c) cs) / mkV (fromIntegral n)
 
--- Given an 
+-- Given a render function and a coordinate,
 antialias :: RandomGen g
           => Config g
           -> ((F3, F3) -> Rand g Color)
           -> Coord
           -> Rand g Color3i
-antialias cfg f coord = do
-    replicateM (nSamples cfg) (coordToUV (screenSize cfg) coord)
-        >>= mapM f
-        >>= pure . scaleColor
-                 . avgColor (nSamples cfg)
+antialias cfg f coord =
+    scaleColor . avgColor (nSamples cfg) <$>
+        (replicateM (nSamples cfg) (coordToUV (screenSize cfg) coord)
+            >>= mapM f)
         
 render :: RandomGen g => Config g -> Rand g [Color3i]
 render cfg
@@ -273,7 +259,7 @@ writePPM path (Dim (nx, ny)) cs = do
 
 gConfig :: RandomGen g => Config g
 gConfig = Config 
-    { screenSize = Dim (400, 200)
+    { screenSize = Dim (200, 100)
     , camera     = Camera
                    { lowerLeft  = V3 (-2) (-1) (-1)
                    , horizontal = V3 4 0 0
